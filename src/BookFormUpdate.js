b157e665130993b8d26c5c0f47f90c358dc39a41
DrPayne25 <alex.payne1125@gmail.com>
2021-08-24 21:46:00 -0700

    created an update component for my BookForm

---
+++ src/BookFormUpdate.js
@@ -0,0 +1,72 @@
+import { withAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+
+class BookFormUpdate extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      _id: this.props.book._id,
+      title: this.props.book.title,
+      description: this.props.book.description,
+      status: this.props.book.status,
+      email: this.props.auth0.user.email,
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('In Handle Submit:', this.state)
+    this.props.handleUpdate(this.state);
+    this.props.handleClose();
+  }
+
+  handleTitle = (e) => {
+    e.preventDefault();
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  handleDescription = (e) => {
+    e.preventDefault();
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  handleStatus = (e) => {
+    e.preventDefault();
+    this.setState({
+      status: e.target.value
+    })
+  }
+  
+
+  render() {
+    console.log('bookFormUpdateState:', this.props.book)
+    return (
+      <Container id='formContainer'>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Label><h2>Update A Book</h2></Form.Label>
+          <Form.Group controlId="title">
+            <Form.Label>Book Title</Form.Label>
+            <Form.Control type='text' onChange={this.handleTitle} value={this.state.title}/>
+          </Form.Group>
+
+          <Form.Group controlId="description">
+            <Form.Label>Book Description</Form.Label>
+            <Form.Control type='text' onChange={this.handleDescription} value={this.state.description}/>
+          </Form.Group>
+
+          <Form.Group controlId="status">
+            <Form.Label>Book Status</Form.Label>
+            <Form.Control type='text' onChange={this.handleStatus} value={this.state.status}/>
+          </Form.Group>
+          <Button type='submit'>Update</Button>
+        </Form>
+      </Container>
+    )
+  }
+}
+
+export default withAuth0(BookFormUpdate);
--- src/Books.js
+++ src/Books.js
@@ -1,9 +1,31 @@
 import React from 'react'
-import { Card, Button, Container } from 'react-bootstrap'
+import { Card, Button, Container, Modal} from 'react-bootstrap'
 import Carousel from 'react-bootstrap/Carousel'
 import './Books.css';
+import BookFormUpdate from './BookFormUpdate'
 
 class Books extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false,
+      selectedBook: null
+    }
+  }
+
+  handleClose = () => {
+    this.setState({
+      showModal: false,
+    })
+  }
+
+  handleShow = (book) => {
+    this.setState({
+      showModal: true,
+      selectedBook: book,
+    });
+  }
+
   renderBooks() {
     let arr = this.props.bookData.map((book, idx) => {
       return (
@@ -14,6 +36,7 @@ class Books extends React.Component {
               <Card.Text>Book Description: {book.description}</Card.Text>
               <Card.Text>Current Status: {book.status}</Card.Text>
               <Card.Text>Current User: {book.email}</Card.Text>
+              <Button id='button' onClick={() => this.handleShow(book)}>Update this</Button>
               <Button id='button' variant="outline-danger" onClick={() => this.props.handleDelete(book._id)}>Delete this</Button>
             </Card.Body>
           </Card>
@@ -23,12 +46,23 @@ class Books extends React.Component {
     return arr;
   }
   render() {
+    console.log('BookForm:', this.state);
     return (
       <>
         <Container>
           <Carousel >
             {this.renderBooks()}
           </Carousel>
+          <Modal show={this.state.showModal} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+              </Modal.Header>
+              <Modal.Body>
+                {
+                  this.state.selectedBook ? 
+                  <BookFormUpdate handleClose={this.handleClose} book={this.state.selectedBook} handleUpdate={this.props.handleUpdate} />
+                : ''}
+              </Modal.Body>
+            </Modal>
         </Container>
       </>
     )
